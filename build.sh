6926599c6af86e2e123404088001a22517ff3518
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-03-07 12:22:44 +1100

    fixed build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir bin
-go build rpi-sshsign -o bin/rpi-sshsign
+# mkdir bin
+go build -o bin/rpi-sshsign ./rpi-sshsign
 cp bin/rpi-sshsign ansible/files/rpi-sshsign
-go build openvpn-mongopass -o bin/openvpn-mongopass
-env GOOS=linux GOARCH=arm GOARM=7 go build -a -o bin/go-eyepi
+go build -o bin/openvpn-mongopass ./openvpn-mongopass
+env GOOS=linux GOARCH=arm GOARM=7 go build -a -o bin/go-eyepi .
 cp bin/go-eyepi ansible/files/go-eyepi
