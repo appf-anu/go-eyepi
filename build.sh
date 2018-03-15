0ee7f138a5610fbb54f444c487f77fedbe011636
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-03-15 13:41:46 +1100

    fixing build for exec format error

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ env GOOS=linux GOARCH=arm GOARM=7 go build -o bin/rpi-sshsign ./rpi-sshsign
 cp bin/rpi-sshsign ansible/files/rpi-sshsign
 env GOOS=linux go test ./openvpn-mongopass
 go build -o bin/openvpn-mongopass ./openvpn-mongopass
-env GOOS=linux GOARCH=arm GOARM=7 go test .
+env GOOS=linux go test .
 
 env GOOS=linux GOARCH=arm GOARM=7 go build -a -o bin/go-eyepi .
 cp bin/go-eyepi ansible/files/go-eyepi
